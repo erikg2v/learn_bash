89e8532ff0d6a89c68fb56e699e576cf1d82422c
erikg2v <erik@g2voptics.com>
2021-05-27 14:26:38 -0600

    fixing git issues

--- lesson_1/2_2_prelim_2.sh
+++ lesson_1/2_2_prelim_2.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-logs=/home/linux90216/Documents/learn_bash 
+logs=/home/linux90216/Documents/learn_bash/lesson_1 
 exec > $logs/mylog.txt
 echo 'Here you go mtt i figured it out'
 date
